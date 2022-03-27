4cba0159d7fee826757667af105f0c46d2eb5cc0
sebastian <zesebasbh100@gmail.com>
2022-03-27 14:14:53 -0600

    NTG004 iguro el cmabio del playlist

--- js/controlador.js
+++ js/controlador.js
@@ -2,29 +2,35 @@
 import {TOKEN} from './generartoken.js'
 import {generaruri} from './generadoruri.js'
 import {consumirAPI} from './servicios.js'
+import {pintarCanciones} from './diseño.js'
 
 console.log(TOKEN)
 
 
-let boton=document.getElementById("boton")
+
+
+let boton=document.getElementById("botn")
 boton.addEventListener("click",function(evento){
     evento.preventDefault()
-    let artistaselecionado=document.getElementById("artista").value
-    console.log("la fruta selecionadad es :" + artistaselecionado)
-    let URI=generaruri(artistaselecionado)
+    let artistas=document.getElementById("artistas").value
+    let URI=generaruri(artistas)
      async function activarAPI(){
         let datosConsultadosAPI=await consumirAPI(URI,TOKEN)
+        pintarCanciones(datosConsultadosAPI)
 
-        console.log(datosConsultadosAPI)
+        
+
+    
 
     }
 
 
-   
+     
+    
     activarAPI()
+    
 
 
 })
-
 
 
