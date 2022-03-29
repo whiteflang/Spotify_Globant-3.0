d8cbe06ec3574ba6eca23b63e868be300959349f
401 <401@academicos.cesde.edu.co>
2022-03-29 11:12:55 -0500

    NTG005 GENERAR tOKEN AUTOMATICO

--- js/controlador.js
+++ js/controlador.js
@@ -1,21 +1,23 @@
 
-import {TOKEN} from './generartoken.js'
+import {generartoken} from './generartoken.js'
 import {generaruri} from './generadoruri.js'
 import {consumirAPI} from './servicios.js'
 import {pintarCanciones} from './diseño.js'
 
-console.log(TOKEN)
 
 
 
 
-let boton=document.getElementById("botn")
+
+let boton=document.getElementById("boton")
 boton.addEventListener("click",function(evento){
     evento.preventDefault()
     let artistas=document.getElementById("artistas").value
     let URI=generaruri(artistas)
      async function activarAPI(){
-        let datosConsultadosAPI=await consumirAPI(URI,TOKEN)
+        let token=await generartoken()
+    
+        let datosConsultadosAPI=await consumirAPI(URI,token)
         pintarCanciones(datosConsultadosAPI)
 
         
--- js/generartoken.js
+++ js/generartoken.js
@@ -1,1 +1,43 @@
-export let TOKEN="Bearer BQAEYYaVJ7OjK2SBXoezS7p9ZDTVbdk_0dj4W71FaUdRZoqzPTAV2Aw9aoPS0Cann_JkQdOhfJ9l9SCN4-dCjBm3FNVgcKA9uBlRmPD25ZyVv-Vd1Ng4-TnM5LDwZ0aNXXzS9nroNHOWNKliamGTp8-XI1wsQCw"
+export async function generartoken(){
+
+    let URI="https://accounts.spotify.com/api/token"
+    let Client_id="client_id=dbcd157fee1a444d84022c05ea178fac"
+    let Client_secret="client_secret=7b74e81046ff4949b47d92e77c9aac5b"
+    let grant_type="grant_type=client_credentials"
+    
+    let parametros={
+        method:"POST",
+        headers:{"content-Type":"application/x-www-form-urlencoded"},
+        body:`${Client_id}&${Client_secret}&${grant_type}`
+    }
+ 
+   /* fetch(URI,parametros)
+    .then(function(respuesta){
+         return(respuesta.json())
+    })
+    .then(function(respuesta){
+                console.log(respuesta.token_type+""+respuesta.access_token)
+                
+    })
+    .catch(function(respuesta){
+                 console.log(respuesta)
+ 
+    })*/
+
+    
+   let respuesta=await fetch(URI,parametros)
+   let infoToken=await  respuesta.json()
+
+   let token=`${infoToken.token_type} ${infoToken.access_token}`
+   
+   return(token)
+
+ }
+
+  generartoken()
+  
+ 
+ 
+ 
+ 
+ 
